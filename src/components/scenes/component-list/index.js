a658919d7cab5853a010da0b6d87c735f5b5fc46
ZauberNerd <zaubernerd@zaubernerd.de>
2017-02-07 01:30:35 +0100

    feat(component-list): render list of components

---
+++ src/components/scenes/component-list/index.js
@@ -0,0 +1,52 @@
+// @flow
+import React, { Component } from 'react';
+import { ListView, Text, View } from 'react-native';
+
+// TODO: define outside and import only type
+type ComponentT = {
+  displayName: string,
+  description: string,
+};
+
+export type ComponentListProps = {
+  components: ComponentT[],
+};
+
+type RenderRowProps = ComponentT;
+
+type DataSource = typeof ListView.DataSource;
+
+export default class ComponentList extends Component {
+  props: ComponentListProps;
+
+  state: { dataSource: DataSource };
+
+  dataSource: DataSource;
+
+  constructor(props: ComponentListProps) {
+    super(props);
+
+    this.dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2,
+    });
+
+    this.state = {
+      dataSource: this.dataSource.cloneWithRows(props.components),
+    };
+  }
+
+  renderRow = ({ displayName }: RenderRowProps) => (
+    <View>
+      <Text>{displayName}</Text>
+    </View>
+  );
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@
 import React, { Component } from 'react';
 import Demo from './components/scenes/demo';
 import FullScreenDemo from './components/scenes/full-screen-demo';
+import ComponentList from './components/scenes/component-list';
+
+import components from '../components';
 
 class App extends Component {
   render() {
     return (
-      <FullScreenDemo />
+      <ComponentList components={components} />
     );
   }
 }
