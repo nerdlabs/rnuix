79d26ad85b39b6366dc04281f1c726e49a9a946e
ZauberNerd <zaubernerd@zaubernerd.de>
2017-02-28 15:55:33 +0100

    chore(DemoHeader): render variations of DemoHeader

---
+++ src/components/molecules/demo-header/demo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import DemoHeader from './';
+
+export default {
+    displayName: 'DemoHeader',
+    description: 'Renders the header bar of a demo tile.',
+    demos: [
+        {
+            title: 'No title',
+            render: () => <DemoHeader />,
+        },
+        {
+            title: 'Short title',
+            render: () => <DemoHeader title="x" />,
+        },
+        {
+            title: 'Long title',
+            render: () => (
+                <DemoHeader title="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod" />
+            ),
+        },
+        {
+            title: 'Full screen mode',
+            render: () => <DemoHeader title="title" isFullScreen />,
+        },
+    ],
+};
