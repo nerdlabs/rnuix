3da54ebe2929e562c8ca6220f9022d9f10155966
ZauberNerd <zaubernerd@zaubernerd.de>
2017-02-28 17:29:02 +0100

    chore(DemoRenderer): render variations of DemoRenderer

--- components.js
+++ components.js
@@ -1,4 +1,5 @@
 export default [
+    require('./src/components/atoms/demo-renderer/demo.js').default,
     require('./src/components/atoms/icon/demo.js').default,
     require('./src/components/molecules/demo-header/demo.js').default,
     require('./src/components/organisms/demo-tile/demo.js').default,
---
+++ src/components/atoms/demo-renderer/demo.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react';
+import { Text } from 'react-native';
+
+import DemoRenderer from './';
+
+export default {
+    displayName: 'DemoRenderer',
+    description: 'The canvas on which the demo content will be rendered.',
+    demos: [
+        {
+            title: 'Normal mode',
+            render: () => <DemoRenderer render={() => <Text>Content</Text>} />,
+        },
+        {
+            title: 'Full screen mode',
+            render: () => (
+                <DemoRenderer
+                    isFullScreen
+                    render={() => <Text>Content</Text>}
+                />
+            ),
+        },
+    ],
+};
