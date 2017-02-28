4b2eae273e92f850bdb5abeeaeac010f71d0411a
ZauberNerd <zaubernerd@zaubernerd.de>
2017-02-28 15:55:33 +0100

    chore(DemoHeader): render variations of DemoHeader

--- components.js
+++ components.js
@@ -1,1 +1,4 @@
-export default [require('./src/components/atoms/icon/demo.js').default];
+export default [
+    require('./src/components/atoms/icon/demo.js').default,
+    require('./src/components/molecules/demo-header/demo.js').default,
+];
