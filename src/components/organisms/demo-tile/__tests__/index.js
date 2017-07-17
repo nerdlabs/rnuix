e4a1b397e809c0670da2108da6bbb0a97bdf5361
Veit Lehmann <levito82@gmail.com>
2017-07-17 17:52:39 +0200

    test(demo-tile): add tests

---
+++ src/components/organisms/demo-tile/__tests__/index.js
@@ -0,0 +1,30 @@
+// @flow
+jest.unmock('../');
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+import DemoTile from '../';
+
+const shallow = new ShallowRenderer();
+const render = jest.fn();
+
+describe('DemoTile', () => {
+    it('renders default', () => {
+        const tree = shallow.render(<DemoTile title="Foo" render={render} />);
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('renders fullscreen', () => {
+        const tree = shallow.render(
+            <DemoTile title="Foo" render={render} isFullScreen />,
+        );
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('calls the passed render function', () => {
+        const tree = renderer.create(<DemoTile title="Foo" render={render} />);
+        expect(render).toHaveBeenCalled();
+    });
+});
