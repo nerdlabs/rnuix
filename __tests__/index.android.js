ea7246f6843043d4710c86cf75b63c552afe150d
Veit Lehmann <levito82@gmail.com>
2017-07-17 18:06:18 +0200

    chore(test): use react-test-renderer instead of enzyme

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import App from '../index.android.js';
 
+const renderer = new ShallowRenderer();
+
 it('renders correctly', () => {
-    const tree = shallow(<App />);
+    const tree = renderer.render(<App />);
     expect(tree).toMatchSnapshot();
 });
--- src/components/atoms/touchable/__tests__/index.js
+++ src/components/atoms/touchable/__tests__/index.js
@@ -2,54 +2,37 @@
 jest.unmock('../');
 
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
-import { shallow } from 'enzyme';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 import Touchable from '../';
 
+const shallow = new ShallowRenderer();
 const onPress = jest.fn();
 
 describe('Touchable', () => {
-    let rendered;
-
-    beforeEach(() => {
-        rendered = shallow(<Touchable onPress={onPress} />);
+    it('renders a View and no Touchable* if no onPress is provided', () => {
+        const tree = shallow.render(<Touchable />);
+        expect(tree).toMatchSnapshot();
     });
 
-    it('should render a View and no Touchable* if no onPress is provided', () => {
-        rendered.setProps({
-            onPress: null,
-        });
-
-        expect(rendered.find(View).length).toBe(1);
-        expect(rendered.find(TouchableOpacity).length).toBe(0);
-        expect(rendered.find('TouchableNativeFeedback').length).toBe(0);
+    it('renders a TouchableOpacity by default if onPress is provided', () => {
+        const tree = shallow.render(<Touchable onPress={onPress} />);
+        expect(tree).toMatchSnapshot();
     });
 
-    it('should render a TouchableOpacity by default if onPress is provided', () => {
-        expect(rendered.find(TouchableOpacity).length).toBe(1);
+    it('renders a TouchableOpacity by default if onLongPress is provided', () => {
+        const tree = shallow.render(<Touchable onLongPress={onPress} />);
+        expect(tree).toMatchSnapshot();
     });
 
-    it('should render a TouchableOpacity by default if onLongPress is provided', () => {
-        rendered.setProps({
-            onPress: null,
-            onLongPress: onPress,
-        });
-
-        expect(rendered.find(TouchableOpacity).length).toBe(1);
+    it('renders a View if onPress and disabled are provided', () => {
+        const tree = shallow.render(<Touchable onPress={onPress} disabled />);
+        expect(tree).toMatchSnapshot();
     });
 
-    it('should render a View if onPress and disabled are provided', () => {
-        rendered.setProps({
-            disabled: true,
-        });
-
-        expect(rendered.find(View).length).toBe(1);
-    });
-
-    it('should handle onPress callback', () => {
-        rendered.simulate('press');
-
+    it('handles onPress callback', () => {
+        const tree = shallow.render(<Touchable onPress={onPress} />);
+        tree.props.onPress();
         expect(onPress).toHaveBeenCalled();
     });
 });
