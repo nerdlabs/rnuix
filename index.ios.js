d78828e47562208608bb918804ba320c3364ffdb
Veit Lehmann <levito82@gmail.com>
2017-10-12 17:41:12 +0200

    chore(react-native): upgrade from 0.48.4 to 0.49.3

--- index.ios.js
+++
@@ -1,6 +0,0 @@
-// @flow
-import { AppRegistry } from 'react-native';
-import createApp from './src';
-import components from './components';
-
-AppRegistry.registerComponent('rnuix', () => createApp(components));
